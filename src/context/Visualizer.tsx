33bba64bf9f7750bbccdf9c812dce96931da0537
Animesh-roy100 <royanimesh7478@gmail.com>
2024-03-03 03:17:29 +0530

    resetArrayAndAnimation added

--- src/context/Visualizer.tsx
+++ src/context/Visualizer.tsx
@@ -2,58 +2,90 @@
 
 import { createContext, useContext, useState } from "react";
 import { SortingAlgorithmType } from "../lib/types";
-import { MAX_ANIMATION_SPEED } from "../lib/utils";
+import {
+  MAX_ANIMATION_SPEED,
+  generateRandomNumberFromInterval,
+} from "../lib/utils";
 
 interface SortingAlgorithmContextType {
-    arrayToSort: number[];
-    setArrayToSort: (array: number[]) => void;
-    selectedAlgorithm: SortingAlgorithmType;
-    setSelectedAlgorithm: (algorithm: SortingAlgorithmType) => void;
-    isSorting: boolean;
-    setIsSorting: (isSorting: boolean) => void;
-    animationSpeed: number;
-    setAnimationSpeed: (speed: number) => void;
-    isAnimationComplete: boolean;
-    setIsAnimationComplete: (isComplete: boolean) => void;
-    resetArrayAndAnimation: () => void;
-    runAnimation: () => void;
-  }
-
-const SortingAlgorithmContext = createContext<SortingAlgorithmContextType | undefined>(undefined)
-
-export const SortingAlgorithmProvider = ({children} : {children: React.ReactNode}) => {
-    const [arrayToSort, setArrayToSort] = useState<number[]>([])
-    const [selectedAlgorithm, setSelectedAlgorithm] = useState<SortingAlgorithmType>("bubble")
-    const [isSorting, setIsSorting] = useState<boolean>(false)
-    const [animationSpeed, setAnimationSpeed] = useState<number>(MAX_ANIMATION_SPEED)
-    const [isAnimationComplete, setIsAnimationComplete] = useState<boolean>(false)
-
-    const resetArrayAndAnimation = () => {}
-
-    const runAnimation = () => {}
-
-    const value = {
-        arrayToSort,
-        setArrayToSort,
-        selectedAlgorithm,
-        setSelectedAlgorithm,
-        isSorting,
-        setIsSorting,
-        animationSpeed,
-        setAnimationSpeed,
-        isAnimationComplete,
-        setIsAnimationComplete,
-        resetArrayAndAnimation,
-        runAnimation
-    }
-
-    return <SortingAlgorithmContext.Provider value={value}>{children}</SortingAlgorithmContext.Provider>
+  arrayToSort: number[];
+  setArrayToSort: (array: number[]) => void;
+  selectedAlgorithm: SortingAlgorithmType;
+  setSelectedAlgorithm: (algorithm: SortingAlgorithmType) => void;
+  isSorting: boolean;
+  setIsSorting: (isSorting: boolean) => void;
+  animationSpeed: number;
+  setAnimationSpeed: (speed: number) => void;
+  isAnimationComplete: boolean;
+  setIsAnimationComplete: (isComplete: boolean) => void;
+  resetArrayAndAnimation: () => void;
+  runAnimation: () => void;
 }
+
+const SortingAlgorithmContext = createContext<
+  SortingAlgorithmContextType | undefined
+>(undefined);
+
+export const SortingAlgorithmProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [arrayToSort, setArrayToSort] = useState<number[]>([]);
+  const [selectedAlgorithm, setSelectedAlgorithm] =
+    useState<SortingAlgorithmType>("bubble");
+  const [isSorting, setIsSorting] = useState<boolean>(false);
+  const [animationSpeed, setAnimationSpeed] =
+    useState<number>(MAX_ANIMATION_SPEED);
+  const [isAnimationComplete, setIsAnimationComplete] =
+    useState<boolean>(false);
+
+  const resetArrayAndAnimation = () => {
+    const contentContainer = document.getElementById("content-container");
+    if (!contentContainer) return;
+
+    const contentContainerWidth = contentContainer.clientWidth;
+    const tempArray: number[] = [];
+    const numLines = contentContainerWidth / 8;
+    const containerHeight = window.innerHeight;
+    const maxLineHeight = Math.max(containerHeight - 420, 100);
+    for (let i = 0; i < numLines; i++) {
+      tempArray.push(
+        generateRandomNumberFromInterval(100, maxLineHeight - 100)
+      );
+    }
+  };
+
+  const runAnimation = () => {};
+
+  const value = {
+    arrayToSort,
+    setArrayToSort,
+    selectedAlgorithm,
+    setSelectedAlgorithm,
+    isSorting,
+    setIsSorting,
+    animationSpeed,
+    setAnimationSpeed,
+    isAnimationComplete,
+    setIsAnimationComplete,
+    resetArrayAndAnimation,
+    runAnimation,
+  };
+
+  return (
+    <SortingAlgorithmContext.Provider value={value}>
+      {children}
+    </SortingAlgorithmContext.Provider>
+  );
+};
 
 export const useSortingAlgorithmContext = () => {
-    const context = useContext(SortingAlgorithmContext)
-    if (!context) {
-        throw new Error("useSortingAlgorithmContext must be used within a SortingAlgorithmProvider")
-    }
-    return context
-}
+  const context = useContext(SortingAlgorithmContext);
+  if (!context) {
+    throw new Error(
+      "useSortingAlgorithmContext must be used within a SortingAlgorithmProvider"
+    );
+  }
+  return context;
+};
